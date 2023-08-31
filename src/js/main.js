e69db3e4a4508db87e7f2debbda77dc277bf7928
tequilex <dron2010xxx@mail.ru>
2023-08-31 16:00:30 +0300

    test

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
+import mask from "./libs/phoneMask/mask"
+import './libs/youtubeLightbox/youtubeLightbox.js'
+
+
 const navBtn = document.querySelector('.nav-icon-btn')
 const iconBtn = document.querySelector('.nav-icon')
 const nav = document.querySelector('.header__top-row')
@@ -7,3 +11,16 @@ navBtn.onclick = () => {
   nav.classList.toggle('header__top-row--mobile');
   document.body.classList.toggle('no-scroll')
 }
+
+mask('[data-tel-input]')
+
+const phoneInputs = document.querySelectorAll('[data-tel-input]')
+
+phoneInputs.forEach((input) => {
+  input.addEventListener('input', () => {
+    if (input.value == '+') input.value = ''
+  })
+  input.addEventListener('blur', () => {
+    if (input.value == '+') input.value = ''
+  })
+})
